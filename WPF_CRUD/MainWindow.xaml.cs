d5eaec94d72f3e404fba1783c319ff0be49ead9d
mrkolobok0220@gmail.com <mrkolobok0220@gmail.com>
2024-08-01 14:30:35 +0300

    First Commit

---
+++ WPF_CRUD/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace WPF_CRUD;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
